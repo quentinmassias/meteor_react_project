5df70c314bd7189c3a07b4802804cd47983d4a42
Quentin Massias <quentin.massias28630@gmail.com>
2017-12-10 14:17:22 +0100

    commit first changes on router, still WIP

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,5 +1,7 @@
-import { FlowRouter } from 'meteor/kadira:flow-router';
-import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+import React from 'react';
+import { Router, Route } from 'react-router';
+import createBrowserHistory from 'history/createBrowserHistory';
+
 
 // Import needed templates
 import '../../ui/layouts/body/body.js';
@@ -8,7 +10,23 @@ import '../../ui/pages/promotions/list.js';
 import '../../ui/pages/students/list.js';
 import '../../ui/pages/not-found/not-found.js';
 
+
+const browserHistory = createBrowserHistory();
+
 // Set up all routes in the app
+
+export const renderRoutes = () => (
+    <Router history={browserHistory}>
+        <div>
+            <Route exact path="/" component={AppContainer}/>
+            <Route path="lists/:id" component={ListPageContainer}/>
+            <Route path="signin" component={AuthPageSignIn}/>
+            <Route path="join" component={AuthPageJoin}/>
+            <Route path="*" component={NotFoundPage}/>
+        </div>
+    </Router>
+);
+
 FlowRouter.route('/', {
     name: 'App.home',
     action() {
